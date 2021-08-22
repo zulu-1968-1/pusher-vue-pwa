53803026c389b77e4db5d317f599dcc46c8a0ead
zulu-1968-1 <harry.blaauw@home.nl>
2021-08-22 15:08:58 +0200

    Initial

---
+++ src/components/Hello.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg  + ' ' + zulu}}</h1>
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hello',
+  data () {
+    return {
+      msg: 'Welcome, ',
+	  zulu: 'By zulu 2021'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style>
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #35495E;
+}
+</style>
